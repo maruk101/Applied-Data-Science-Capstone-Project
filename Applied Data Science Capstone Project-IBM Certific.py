82feb1963cbe8471a79e5056fc715b95972fb0b7
maruk101 <62465340+maruk101@users.noreply.github.com>
2021-01-31 11:36:08 +0100

    Applied-Data-Science-Capstone-Project
    From Watson Studio

---
+++ Applied Data Science Capstone Project-IBM Certific.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Applied Data Science Capstone Project</h1>
+# 
+# This Jupyter Notebook wil be used to execute the Applied Data Science Capstone Project in IBM's Data Science/ Python Certificate.
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
